5d439fdf8f3ce6713136f6f5665ac99ed320ff3d
vidya sagar <sagar4226@gmail.com>
2018-05-30 01:06:12 -0400

    made changes

--- sendText.py
+++ sendText.py
@@ -8,13 +8,13 @@ Created on Fri Jan 12 13:41:50 2018
 from twilio.rest import TwilioRestClient
 
 # Your Account SID from twilio.com/console
-account_sid = "ACd7875ab9254a2b737c1103382c7d2d7c"
+account_sid = "Your account ID"
 # Your Auth Token from twilio.com/console
-auth_token = "1c722157fbe91d652acac32fc87060b9"
+auth_token = "Your auth token"
 client = TwilioRestClient(account_sid, auth_token)
 message = client.messages.create(
-    to="+12013817554",
-    from_="++16157634107",
+    to="Your phone Number",
+    from_="Twillo phone",
     body="Hello from Python!")
 
 print(message.sid)
